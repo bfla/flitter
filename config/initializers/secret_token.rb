1b75578cbb6ff96066ee6a6bc67dc8e076bec679
bfla <bfla@umich.edu>
2014-07-03 18:36:32 -0400

    installed bundle including rspec and Capybara

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flitter::Application.config.secret_key_base = 'af624758dfc23d3c5f843ada124250c1a19f7a565070be699a6e31bd54b61ef922ec00e39f02b31831856b3aee17105e1412ebd3cf00e1e837cf0f02f52ba92a'
